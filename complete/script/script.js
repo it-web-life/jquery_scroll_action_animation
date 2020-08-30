9c8a552aa52a4da20f82c8dd66e7c467b71c8654
it-web-life <info@it-web-life.com>
2020-08-30 16:31:31 +0900

    :sparkles: feat: スクロール動作に連動してアニメーションするjQurey&CSS実装

---
+++ complete/script/script.js
@@ -0,0 +1,46 @@
+$(function() {
+  // 固定する要素を取得
+  var $targetElements = $('.js-fade-animation');
+
+  // 固定する要素があるとき
+  if ($targetElements.length) {
+    // 固定する要素の高さを取得(複数ある場合を考えて配列で保存)
+    var fixedOffsets = $targetElements.map(function(index, element) {
+      return $(element).offset().top;
+    });
+    // windowを取得
+    var $window = $(window);
+    // 画面の高さを取得
+    var windowHight = $window.height();
+
+    // resizeイベント
+    $window.on('resize', $.throttle(250, function() {
+      // 画面の高さを再取得
+      windowHight = $window.height();
+
+      // Offsetを再取得
+      fixedOffsets = $targetElements.map(function(index, element) {
+        return $(element).offset().top;
+      });
+    }));
+
+    // スクロールイベント
+    $window.on('scroll', $.throttle(250, function() {
+      // スクロール位置を取得
+      var scrollTop = $(this).scrollTop();
+
+      // .js-fade-animationの要素をすべてチェックする
+      fixedOffsets.each(function(index, offset) {
+        // 固定要素の位置よりも下にスクロールされているとき
+        if (scrollTop + windowHight > offset) {
+          // 要素を固定する
+          $targetElements.eq(index).addClass('is-show');
+
+          // アニメーション後は監視対象から外す
+          fixedOffsets.splice(index, 1);
+          $targetElements.splice(index, 1);
+        }
+      });
+    }));
+  }
+});
