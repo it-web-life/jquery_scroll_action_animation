5f3d1823ccfe316d2142fc8813e310d795ece85c
it-web-life <info@it-web-life.com>
2020-08-30 18:12:06 +0900

    :recycle: refactor: コメントやCSSを微調整

--- complete/script/script.js
+++ complete/script/script.js
@@ -1,10 +1,10 @@
 $(function() {
-  // 固定する要素を取得
+  // アニメーションさせる要素を取得
   var $targetElements = $('.js-fade-animation');
 
-  // 固定する要素があるとき
+  // アニメーションさせる要素があるとき
   if ($targetElements.length) {
-    // 固定する要素の高さを取得(複数ある場合を考えて配列で保存)
+    // アニメーションさせる要素の高さを取得(複数ある場合を考えて配列で保存)
     var fixedOffsets = $targetElements.map(function(index, element) {
       return $(element).offset().top;
     });
@@ -31,9 +31,9 @@ $(function() {
 
       // .js-fade-animationの要素をすべてチェックする
       fixedOffsets.each(function(index, offset) {
-        // 固定要素の位置よりも下にスクロールされているとき
+        // アニメーションさせる要素の位置よりも下にスクロールされているとき
         if (scrollTop + windowHight > offset) {
-          // 要素を固定する
+          // 要素をアニメーションさせる (.is-showを付与)
           $targetElements.eq(index).addClass('is-show');
 
           // アニメーション後は監視対象から外す
